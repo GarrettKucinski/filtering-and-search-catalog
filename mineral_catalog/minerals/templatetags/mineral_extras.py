91b885401950611db863b906802340ea854892a9
Garrett Kucinski <garrettryan@doejo.com>
2018-01-29 20:10:16 -0700

    added show random mineral functionality

--- mineral_catalog/minerals/templatetags/mineral_extras.py
+++ mineral_catalog/minerals/templatetags/mineral_extras.py
@@ -1,4 +1,6 @@
 from django import template
+from random import randint
+from minerals.models import Mineral
 
 register = template.Library()
 
@@ -6,3 +8,9 @@ register = template.Library()
 @register.filter
 def replace_underscores_with_space(key):
     return ' '.join(key.split('_'))
+
+
+@register.simple_tag
+def generate_random_num():
+    mineral_count = Mineral.objects.all().count()
+    return randint(1, mineral_count)
--- mineral_catalog/minerals/views.py
+++ mineral_catalog/minerals/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 
 from .models import Mineral
+from collections import OrderedDict
 
 # Create your views here.
 
 
 def mineral_detail(request, pk):
     mineral = Mineral.objects.values().get(pk=pk)
+    mineral.pop('id', None)
+
+    mineral = OrderedDict(
+        sorted(mineral.items(), key=lambda x: x[1], reverse=True))
+
     return render(request, 'mineral_detail.html',
                   {'mineral': mineral, 'detail': True})
